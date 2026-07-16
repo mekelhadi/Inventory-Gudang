b5e5751c3f86e4d3cfe769bb4941eea8c5b5540b
ayapremiumisme-tech <ayapremiumisme@gmail.com>
2026-07-16 20:47:37 +0700

    fix: force HTTPS detection for Vercel so form submissions use https

--- api/index.php
+++ api/index.php
@@ -2,4 +2,14 @@
 
 error_reporting(E_ALL & ~E_DEPRECATED);
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+if (!isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on')) {
+    if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'vercel.app') !== false) {
+        $_SERVER['HTTPS'] = 'on';
+    }
+}
+
 require __DIR__ . '/../public/index.php';
